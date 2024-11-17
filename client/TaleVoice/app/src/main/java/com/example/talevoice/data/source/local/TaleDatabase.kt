cc859c2270248ece8ddabedbb0f59a33b4ea3db7
Jospeh Jingi Jung <jingi.jung@kaist.ac.kr>
2024-11-17 15:10:27 +0900

    update TaleDatabase using Converter

--- client/TaleVoice/app/src/main/java/com/example/talevoice/data/source/local/TaleDatabase.kt
+++ client/TaleVoice/app/src/main/java/com/example/talevoice/data/source/local/TaleDatabase.kt
@@ -2,8 +2,25 @@ package com.example.talevoice.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import java.util.Date
 
 @Database(entities = [LocalTaleListItem::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class TaleDatabase : RoomDatabase() {
     abstract fun taleDao(): TaleDao
+}
+
+class Converters {
+
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
 }
