182cf92401759679b48a047009aaca0603eb9a5e
Jospeh Jingi Jung <jingi.jung@kaist.ac.kr>
2024-11-17 15:11:53 +0900

    log and test TaleRepository apis

--- client/TaleVoice/app/src/main/java/com/example/talevoice/ui/talelist/TaleListFragment.kt
+++ client/TaleVoice/app/src/main/java/com/example/talevoice/ui/talelist/TaleListFragment.kt
@@ -2,12 +2,16 @@ package com.example.talevoice.ui.talelist
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 
 import com.example.talevoice.R
+import kotlinx.coroutines.launch
 
 class TaleListFragment : Fragment() {
 
@@ -15,7 +19,9 @@ class TaleListFragment : Fragment() {
         fun newInstance() = TaleListFragment()
     }
 
-    private val viewModel: TaleListViewModel by viewModels()
+    private val viewModel: TaleListViewModel by viewModels {
+        ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +33,20 @@ class TaleListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_tale_list, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_tale_list, container, false)
+
+        viewModel.taleList.observe(viewLifecycleOwner) { taleList ->
+            Log.d("TaleListFragment", "taleList update")
+            Log.d("TaleListFragment", taleList.toString())
+        }
+
+        lifecycleScope.launch {
+            viewModel.getTaleDetail("1").let {
+                Log.d("TaleListFragment", it.toString())
+            }
+        }
+
+        return rootView
     }
 
 }
--- client/TaleVoice/app/src/main/java/com/example/talevoice/ui/talelist/TaleListViewModel.kt
+++ client/TaleVoice/app/src/main/java/com/example/talevoice/ui/talelist/TaleListViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.talevoice.ui.talelist
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.talevoice.TaleApplication
+import com.example.talevoice.data.TaleItem
+import com.example.talevoice.data.TaleListItem
+import com.example.talevoice.data.TaleRepository
 
-class TaleListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class TaleListViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: TaleRepository =
+        (application as TaleApplication).taleRepository
+
+    val taleList: LiveData<List<TaleListItem>> = repository.getTaleList()
+
+    suspend fun getTaleDetail(taleId: String) :TaleItem {
+        return repository.getTaleItem(taleId)
+    }
 }
