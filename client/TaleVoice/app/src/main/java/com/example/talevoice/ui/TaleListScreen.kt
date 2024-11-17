9a458393727f453c2057744bc51540acce865071
Jospeh Jingi Jung <jingi.jung@kaist.ac.kr>
2024-11-17 23:16:05 +0900

    remove unused import

--- client/TaleVoice/app/src/main/java/com/example/talevoice/ui/TaleListScreen.kt
+++ client/TaleVoice/app/src/main/java/com/example/talevoice/ui/TaleListScreen.kt
@@ -1,7 +1,5 @@
 package com.example.talevoice.ui
 
-import android.os.Bundle
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
