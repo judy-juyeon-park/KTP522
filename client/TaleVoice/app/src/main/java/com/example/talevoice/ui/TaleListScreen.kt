c6ea5f172ecadbd15de948fdf3436bad2c6819ab
Eunyoung Lee <eylee928@kaist.ac.kr>
2024-12-13 19:27:31 +0900

    Make loading dialog when it uses creating tale

--- client/TaleVoice/app/src/main/java/com/example/talevoice/ui/TaleListScreen.kt
+++ client/TaleVoice/app/src/main/java/com/example/talevoice/ui/TaleListScreen.kt
@@ -1,9 +1,11 @@
 package com.example.talevoice.ui
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,11 +22,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -33,21 +39,59 @@ import com.example.talevoice.TaleList
 import com.example.talevoice.viewmodel.TaleIllustrationViewModel
 import com.example.talevoice.viewmodel.TaleListViewModel
 import com.example.talevoice.viewmodel.TaleListViewModelFactory
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 @Composable
-fun TaleListScreen(navController: NavHostController, taleIllustrationViewModel: TaleIllustrationViewModel, name: String?, gender: String?) {
+fun TaleListScreen(
+    navController: NavHostController,
+    taleIllustrationViewModel: TaleIllustrationViewModel,
+    name: String?,
+    gender: String?
+) {
     val repository = (LocalContext.current.applicationContext as TaleApplication).taleRepository
     val viewModel: TaleListViewModel = viewModel(
         factory = TaleListViewModelFactory(repository)
     )
 
     var isLoading by remember { mutableStateOf(false) }
+    var isCreating by remember { mutableStateOf(false) }
     val taleList by viewModel.taleList.collectAsState()
 
+    if (isCreating) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Gray.copy(alpha = 0.5f))
+                .clickable(enabled = true, onClick = {}) // 터치 차단
+        )
+        Dialog(onDismissRequest = { /* Do nothing */ }) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "${name ?: "사용자"}의 이야기를 만들고 있습니다.",
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontFamily = FontFamily.Serif,
+                        fontWeight = FontWeight.Thin,
+                        color = Color.White,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                    LinearProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxWidth(0.8f)
+                            .height(4.dp)
+                    )
+                }
+            }
+        }
+    }
 
     Column {
         Column(
@@ -65,12 +109,12 @@ fun TaleListScreen(navController: NavHostController, taleIllustrationViewModel: 
 
             Button(
                 onClick = {
-                    isLoading = true // 로딩 시작
+                    isCreating = true
                     Log.d("TaleListScreen", "Button clicked")
 
                     viewModel.viewModelScope.launch {
                         try {
-                            isLoading = true
+                            isCreating = true
                             val safeName = name ?: "Unknown"
                             val safeGender = gender ?: "Unknown"
                             // 동화 내용 + 삽화 1개 리턴
@@ -79,15 +123,15 @@ fun TaleListScreen(navController: NavHostController, taleIllustrationViewModel: 
                             Log.d("TaleListScreen", "prompts ready")
                             taleIllustrationViewModel.fetchIllustrations(prompts)
                             taleIllustrationViewModel.illustrations.first { it.isNotEmpty() }
-                            Log.d("TaleListScreen", "one image ready ready")
+                            Log.d("TaleListScreen", "one image ready")
 
-                            isLoading = false
+                            isCreating = false
                             navController.navigate(taleItem) {
                                 popUpTo<TaleList>()
                             }
 
                         } catch (e: Exception){
-                            isLoading = false
+                            isCreating = false
                             println("Error fetching tale details: ${e.message}")
                         }
                     }
@@ -105,7 +149,7 @@ fun TaleListScreen(navController: NavHostController, taleIllustrationViewModel: 
             }
         }
 
-        if (isLoading) {
+        if (isLoading && !isCreating) {
             LinearProgressIndicator(
                 modifier = Modifier
                     .fillMaxWidth()
